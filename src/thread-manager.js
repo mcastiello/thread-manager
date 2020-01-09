e6fcb27413cc5dba954f9d13702fb5712d92b30d
Marco Castiello <marco.castiello@gmail.com>
2020-01-09 13:23:56 +0000

    Added JSDoc

--- src/thread-manager.js
+++ src/thread-manager.js
@@ -1,26 +1,56 @@
+/*
+ * Copyright (c) 2020
+ * Author: Marco Castiello
+ * E-mail: marco.castiello@gmail.com
+ * Project: ThreadManagerService.js
+ */
 
 import WebThread from "./web-thread";
 
-let threadCount = 0;
-
+/**
+ * Maximum number of threads that can run at the same time.
+ * @type {Number}
+ */
 let threadLimit = 8;
 
+/**
+ * Data object shared among the running threads.
+ * @type {Object}
+ */
 const sharedMemory = {};
 
+/**
+ * Data proxy used to detect when property in the shared memory is changed.
+ * @type {Proxy}
+ */
 const sharedMemoryProxy = new Proxy(sharedMemory, {
     set: (ref, prop, val) => updateSharedMemory(prop, val)
 });
 
+/**
+ * List of callbacks awaiting for an execution slot.
+ * @type {Array}
+ */
 const threadQueue = [];
 
+/**
+ * List of threads currently running.
+ * @type {Array}
+ */
 const threadRunning = [];
 
+/**
+ * Map associating each callback with the generated internal URL used to launch a WebThread.
+ * @type {Map}
+ */
 const threadMap = new WeakMap();
 
-const stopFunction = function() {
-    self.postMessage("thread-terminate");
-};
-
+/**
+ * This method is actually prepended to any callback before being executed as a web thread.
+ * Its purpouse is to add a shared memory area that the thread can access.
+ * This "shared" memory will be accessible both from the manager and from all the running threads.
+ * @private
+ */
 const createSharedMemory = function() {
     let data = {};
     
@@ -50,6 +80,12 @@ const createSharedMemory = function() {
     });
 };
 
+/**
+ * Update the shared memory in the threads and in the manager.
+ * @param {String} property
+ * @param {*} value
+ * @private
+ */
 const updateSharedMemory = (property, value) => {
     sharedMemory[property] = value;
     for (let thread of threadRunning) {
@@ -60,26 +96,45 @@ const updateSharedMemory = (property, value) => {
     }
 };
 
+/**
+ * When a thread is terminated, it is removed from the running 
+ * list and the next one in the que is executed.
+ * @param {WebThread} thread
+ * @private
+ */
 const threadTerminated = thread => {
     const index = threadRunning.indexOf(thread);
     
     if (index >= 0) {
         threadRunning.splice(index, 1);
-        threadCount--;
         
         updateThreadQueue();
     }
     
 };
 
+/**
+ * If there is an execution slot available, it tries to run the next thread in the queue.
+ * @private
+ */
 const updateThreadQueue = () => {
-    if (threadQueue.length > 0 && threadCount < threadLimit) {
+    if (threadQueue.length > 0 && threadRunning.length < threadLimit) {
         const params = threadQueue.shift();
         
         executeThread(...params);
     }
 };
 
+/**
+ * Convert the provided callback into a string (adding the shared memory 
+ * and the stop method) and it converts it into a Blob object.
+ * It will return a URL to the locally stored object.
+ * The URL will only generated once for each callback, if the thread is executed 
+ * more than once, the same URL will be returned.
+ * @param {Function} threadFunction
+ * @returns {String}
+ * @private
+ */
 const loadThreadUrl = threadFunction => {
     let url;
     
@@ -88,7 +143,7 @@ const loadThreadUrl = threadFunction => {
             url = threadMap.get(threadFunction);
         } else {
             const shared = "(" + createSharedMemory.toString() + ")();";
-            const stop = "self.stop=function(){" + stopFunction.toString() + "};";
+            const stop = "self.stop=d=>postMessage({type:'thread-terminate',value:d);";
             const func = "(" + threadFunction.toString() + ")();";
             const blob = new Blob([shared+stop+func]);
 
@@ -100,6 +155,11 @@ const loadThreadUrl = threadFunction => {
     return url;
 };
 
+/**
+ * Create and start a new thread using the provided callback.
+ * @param {Function} threadFunction
+ * @private
+ */
 const createThread = threadFunction => {
     const url = loadThreadUrl(threadFunction);
     
@@ -107,7 +167,10 @@ const createThread = threadFunction => {
         try {
             const thread = new WebThread(url);
 
+            // Listen for the termination event.
             thread.addEventListener("terminate", event => threadTerminated(thread));
+            
+            // Listen for any memory updates.
             thread.addEventListener("message", event => {
                 if (event.data && event.data.type && event.data.type === "shared-memory-updated") {
                     event.stopImmediatePropagation();
@@ -115,6 +178,7 @@ const createThread = threadFunction => {
                 }
             });
             
+            // Update the thread shared memory.
             thread.postMessage({
                 type: "shared-memory-init",
                 value: sharedMemory
@@ -122,13 +186,19 @@ const createThread = threadFunction => {
 
             threadRunning.push(thread);
 
-            threadCount++;
-
             return thread;
         } catch {}
     }
 };
 
+/**
+ * Try to create a new thread and resolve the promise.
+ * If the creation fails, the promise will be rejected.
+ * @param {Function} threadFunction
+ * @param {Function} resolve
+ * @param {Function} reject
+ * @private
+ */
 const executeThread = (threadFunction, resolve, reject) => {
     const thread = createThread(threadFunction);
 
@@ -140,42 +210,94 @@ const executeThread = (threadFunction, resolve, reject) => {
     }
 };
 
+/**
+ * Defaine the manager used to run and handle all the threads.
+ * @type {ThreadManafer}
+ */
 class ThreadManager {
 
+    /**
+     * Get access to the thread shared memory.
+     * @returns {Proxy}
+     */
+    get shared() {
+        return sharedMemoryProxy;
+    }
+
+    /**
+     * Get the count of running threads.
+     * @returns {Number}
+     */
+    get count() {
+        return threadRunning.length;
+    }
+
+    /**
+     * Get a list of running threads.
+     * @returns {Array}
+     */
+    get running() {
+        return threadRunning.slice();
+    }
+    
+    /**
+     * Get the maximum number of threads that can run at the same time.
+     * @returns {Number}
+     */
+    get limit() {
+        return threadLimit;
+    }
+    
+    /**
+     * Set the maximum number of threads that can run at the same time.
+     * The value cannot be less than 1 and reducing the amount of maximum thread won't cause 
+     * the currently running one to stop, it will just make the wait for the ones in the queue 
+     * a bit longer.
+     * @param {Number} value
+     */
+    set limit(value) {
+        value = Number(value);
+        
+        if (!isNaN(value) && value >= 1) {
+            threadLimit = Math.round(value);
+        }
+    }
+
+    /**
+     * Initialise a thread using the provided callback. If there are no execution 
+     * slots available, the callback will be put in a queue and the promise will only 
+     * be resolved when the thread is actually started.
+     * @param {function} threadFunction
+     * @returns {Promise<WebThread>}
+     */
     run(threadFunction) {
         return new Promise((resolve, reject) => {
-            if (threadCount < threadLimit) {
+            if (threadRunning.length < threadLimit) {
                 executeThread(threadFunction, resolve, reject);
             } else {
                 threadQueue.push([threadFunction, resolve, reject]);
             }
         });
     }
-
-    get shared() {
-        return sharedMemoryProxy;
-    }
-
-    get count() {
-        return threadCount;
-    }
-
-    get running() {
-        return threadRunning;
-    }
     
-    get limit() {
-        return threadLimit;
-    }
-    
-    set limit(value) {
-        value = Number(value);
+    /**
+     * Execute the callback asynchronously in a separate thread. 
+     * The promise will be resolved with the value passed to stop method inside the thread.
+     * @param {function} threadFunction
+     * @returns {Promise<*>}
+     */
+    async execute(threadFunction) {
+        const thread = await this.run(threadFunction);
+        const result = await new Promise(resolve => {
+            thread.addEventListener("terminate", event => resolve(event.result));
+        });
         
-        if (!isNaN(value) && value > 0) {
-            threadLimit = Math.round(value);
-        }
+        return result;
     }
     
+    /**
+     * Kill all the running threads.
+     */
     purge() {
         threadQueue = [];
         for (let thread of threadRunning) {
